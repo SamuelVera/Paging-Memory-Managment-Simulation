91b756cc33e936f292fc997ef87e004c0f3b3c78
SamuelVera <samuelvera1999@gmail.com>
2018-11-13 23:58:23 -0400

    Clase Página

---
+++ src/logica/Pagina.java
@@ -0,0 +1,13 @@
+package logica;
+
+public class Pagina {
+    boolean p; //Presente en memoria principal
+    boolean u; //Referenciado recientemente
+    int numMar; //Número de marco asociado
+    int tam; //Tamaño de página
+    
+    Pagina(){
+        
+    }
+    
+}
