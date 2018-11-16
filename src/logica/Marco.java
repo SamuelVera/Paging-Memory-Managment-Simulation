cbdf474465a4321e509d8f396a2e9c08943ceccc
SamuelVera <samuelvera1999@gmail.com>
2018-11-16 04:07:43 -0400

    Clase Marco

---
+++ src/logica/Marco.java
@@ -0,0 +1,38 @@
+package logica;
+
+public class Marco {
+    
+    private ETP pagina;
+    private int numMarco;
+    private boolean libre;
+    
+    protected Marco(int numMarco){
+        this.numMarco = numMarco;
+        this.libre = true;
+    }
+    
+    protected int getNumMarco(){
+        return this.numMarco;
+    }
+    
+    protected ETP getPagina(){
+        if(!this.libre){
+            return this.pagina;
+        }else{
+            return null;
+        }
+    }
+    
+    protected boolean getLibre(){
+        return this.libre;
+    }
+    
+    protected void setPagina(ETP p){
+        this.pagina = p;
+    }
+    
+    protected void setLibre(boolean p){
+        this.libre = p;
+    }
+    
+}
