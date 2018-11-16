e3dd2c0fb0f1567fee929414a290dee41532634c
SamuelVera <samuelvera1999@gmail.com>
2018-11-16 04:08:19 -0400

    Agreagado expulsar a un proceso completamente de memoria e ingresar uno con memoria llena o parcialmente llena

--- src/logica/PlanificadorMid.java
+++ src/logica/PlanificadorMid.java
@@ -3,175 +3,141 @@ package logica;
 import UI.UIEjecucion;
 import java.awt.Color;
 import java.util.LinkedList;
-import java.util.concurrent.Semaphore;
 
-public class PlanificadorMid {
+public class PlanificadorMid { //Planificador de memoria
     
-    private boolean[] marcos; //Marcos en memoria principal
-    private int apuntador = 0;
-    private ETP[] paginasEnMp; //Páginas en memoria principal
-    private LinkedList procesos = new LinkedList(); //Global de los proceso
-    private double tamMP; //Tamaño de la memoria principal
-    private double tamMS; //Tamaño de la memoria secundaria
-    private double tamMarco;
-    static Semaphore accessMem = new Semaphore(1);
+    protected static int apuntador;
     
-    public PlanificadorMid(int numMarcos, double tamMP, double tamMS, double tamMarco){
-        this.marcos = new boolean[numMarcos];
-        this.paginasEnMp = new ETP[numMarcos];
-        for(int i=0;i<this.marcos.length;i++){
-            this.paginasEnMp[i] = null; //Comienza vacio
-            this.marcos[i] = true; //Inicializar como marcos vacios
-        }
-        this.tamMP = tamMP;
-        this.tamMS = tamMS;
-        this.tamMarco = tamMarco;
-    }
+    public PlanificadorMid(){}
     
         //Crear proceso e ingresarlo todo en memoria principal
-        //De no poderse al menos ingresar su mitad
-    public void crearProceso(String id, double tam) throws InterruptedException{
-            //Crear el proceso
-        Proceso p = new Proceso(id, tam, this.tamMarco);
-            //Validar que es creable el proceso
+        //De no poderse expulsar otro proceso.
+    public static void cargarProceso(Proceso p) throws InterruptedException{
             //Al menos la mitad de las páginas caben en mp
             //Queda espacio en memoria secundaria
-        if((p.getCantidadPag()<(2*this.getNumMarcos())) && p.getTam()<=this.getRestanteMs()){ 
-            int aux = this.getMarcosVacios(); //Cantidad de marcos vacíos
-            if(aux != 0){
-                int i=0; //Número de página
-                int j=0; //Número de marco
-                //Verificar si hay marcos suficientes para acomodar el proceso
-                if(aux > p.getCantidadPag()){ //Hay más marcos que páginas del proceso
-                        //Entra completamente en mp
-                    while(i<p.getCantidadPag()){
-                        if(this.marcos[j]){
-                            this.marcos[j] = false; //Este marco está ocupado
-                            UIEjecucion.celdas[j].setText(p.getId()+"/"+i); //Visual
-                            UIEjecucion.celdas[j].setBackground(Color.red); //Llenar el espacio
-                            p.setMarcoToETP(j, i); //Darle el número de marco a la ETP
-                            p.getETP(i).setP(true); //Bit de presencia
-                            p.getETP(i).u = false; //Bit de referencia para reloj
-                            this.paginasEnMp[i] = p.getETP(i); //Nueva página en mp
-                            i++;
-                        }
-                        j++;
+        int aux = PlanificadorMid.getMarcosVacios(); //Cantidad de marcos vacíos
+        if(aux != 0){
+            int i=0; //Número de página
+            int j=0; //Número de marco
+            //Verificar si hay marcos suficientes para acomodar el proceso
+            if(aux > p.getCantidadPag()){ //Hay más marcos que páginas del proceso
+                    //Entra completamente en mp
+                while(i<p.getCantidadPag()){
+                    if(OS.marcos[j].getLibre()){
+                        UIEjecucion.celdas[j].setText(p.getIdP()+"/"+p.getETP(i).numPag); //Visual
+                        UIEjecucion.celdas[j].setBackground(Color.red); //Llenar el espacio
+                        p.setMarcoToETP(j, i); //Darle el número de marco a la ETP
+                        p.getETP(i).setP(true); //Bit de presencia
+                        p.getETP(i).u = true; //Bit de referencia para reloj
+                        OS.marcos[j].setLibre(false); //Este marco está ocupado
+                        OS.marcos[j].setPagina(p.getETP(i)); //Nueva página en el marco
+                        i++;
                     }
-                    p.setEstado(true, false, false); //Estado activo
-                }else{
-                    if((p.getCantidadPag()/aux) <= 2){ //Se puede acomodar al menos la mitad de páginas en memoria principal
-                            //Entra parcialmente
-                        while(i<aux){
-                            if(this.marcos[j]){
-                                this.marcos[j] = false; //Este marco está ocupado
-                                UIEjecucion.celdas[j].setText(p.getId()+"/"+i); //Visual
-                                UIEjecucion.celdas[j].setBackground(Color.red); //Llenar el espacio
-                                p.setMarcoToETP(j, i); //Darle el número de marco a la ETP
-                                p.getETP(i).setP(true); //Bit de presencia
-                                p.getETP(i).u = false; //Bit de referencia para reloj
-                                this.paginasEnMp[i] = p.getETP(i); //Nueva página en mp
-                                i++;
-                            }
-                            j++;
-                        }
-                        p.setEstado(true, false, false);
-                    }else{
-                        //Ingreso apropiativo
-                        this.ingresoApropiativo(p);
-                        p.setEstado(true, false, false);
-                    }
+                    j++;
                 }
             }else{
-                //Ingreso apropiativo
-                this.ingresoApropiativo(p);
-                p.setEstado(true, false, false);
+                    //Ingreso apropiativo
+                PlanificadorMid.ingresoApropiativo(p);
             }
-            this.procesos.add(p);
-            UIEjecucion.ocupado += p.getTam();
-            UIEjecucion.labelIdProcesos.add(p.getId());
         }else{
-            System.out.println("Add JOptinonPane de ms insuficiente o proceso muy grande");
+            //Ingreso apropiativo
+            PlanificadorMid.ingresoApropiativo(p);
         }
+        p.setEstado(true, false, false); //Estado listo
     }
     
         //Supender un proceso
-    public void suspProceso(){
-        
-    }
-    
-        //Eliminar proceso del sistema
-    public void elimProceso(){
+    protected void suspProceso(){
         
     }
     
         //Sacar paginas de un proceso de memoria principal y memoria secundaria
-    private void sacarProcesoMem(){
-        
+    protected static void sacarProcesoMem(Proceso p){
+        for(int i=0;i<OS.marcos.length;i++){
+            if(OS.marcos[i].getPagina() != null){
+                if(OS.marcos[i].getPagina().idProceso.equals(p.getIdP())){
+                    UIEjecucion.celdas[p.getETP(OS.marcos[i].getPagina().numPag).numMar].setBackground(Color.green);
+                    UIEjecucion.celdas[p.getETP(OS.marcos[i].getPagina().numPag).numMar].setText("");
+                    p.getETP(OS.marcos[i].getPagina().numPag).setP(false); //Bit de presencia = 0
+                    p.getETP(OS.marcos[i].getPagina().numPag).u = false; //Bit de referencia 
+                    p.setMarcoToETP(-1, OS.marcos[i].getPagina().numPag); //Quitarle marco a la página
+                    OS.marcos[i].setLibre(true); //Libera el marco
+                    OS.marcos[i].setPagina(null); //Sacar de mp la página
+                }
+            }
+        }
     }
     
         //Ingresar apropiandose de la memoria de otros procesos
-    private void ingresoApropiativo(Proceso p){
+        //2 casos
+        //1: Si hay más memoria que la mitad de sus paginas se cargan cuantas se puedan
+        //2: Si no se cumple el (1) se apropia de marcos de otros páginas de otros procesos hasta cargar su mitad
+    private static void ingresoApropiativo(Proceso p){
         Object[] aux;
-        aux = this.liberarMarcos(p.getCantidadPag()/2);
-            //Se van a cargar la mitad de las páginas del proceso
+        aux = PlanificadorMid.liberarMarcos(p.getCantidadPag());
+            //Se van a cargar las páginas del proceso para ejecutarlo
         for(int i=0;i<aux.length;i++){
             p.getETP(i).setP(true); //Asignar bit de presencia
             p.getETP(i).u = true; //Asignar bit de referencia
-            p.getETP(i).numMar = (int)aux[i]; //Asignar número de marco
-            this.marcos[(int)aux[i]] = false; //Este marco está ocupado
-            UIEjecucion.celdas[(int)aux[i]].setText(p.getId()+"/"+i); //Visual
+            p.setMarcoToETP((int)aux[i], i); //Asignar número de marco
+            OS.marcos[(int)aux[i]].setLibre(false); //Este marco ahora está ocupado
+            UIEjecucion.celdas[(int)aux[i]].setText(p.getIdP()+"/"+p.getETP(i).numPag); //Visual
             UIEjecucion.celdas[(int)aux[i]].setBackground(Color.red); //Llenar el espacio
+            OS.marcos[(int)aux[i]].setPagina(p.getETP(i)); //Nueva página en mp
         }
     }
     
         //Algoritmo del reloj que retorna el número de los marcos liberados
-    private Object[] liberarMarcos(int n){
+    private static Object[] liberarMarcos(int n){
             //Recorrer el buffer//Se liberan n marcos
+        if(n > OS.getNumMarcos()){ //El proceso tiene más páginas que marcos en memoria
+            n = n/2;
+        }
         Object[] aux;
         LinkedList marco = new LinkedList();
             //Antes de recorrer tomar los marcos vacios
-        for(int i=0;i<this.marcos.length;i++){
-            if(this.marcos[i]){
+        for(int i=0;i<OS.marcos.length;i++){
+            if(OS.marcos[i].getLibre()){
                 marco.add(i);
             }
         }
         while(true){
                 //Ya se tienen los n marcos
-            if(n == marco.size()){
+            if(marco.size() >= n){
                 aux = marco.toArray();
                 return aux;
             }
-            if(this.paginasEnMp[this.apuntador] != null){
-                if(this.paginasEnMp[this.apuntador].u){
-                    this.paginasEnMp[this.apuntador].u = false;
+            if(!OS.marcos[PlanificadorMid.apuntador].getLibre()){
+                if(OS.marcos[PlanificadorMid.apuntador].getPagina().u){
+                    OS.marcos[PlanificadorMid.apuntador].getPagina().u = false;
                 }else{
-                    System.out.println(n);
-                    marco.add(this.paginasEnMp[this.apuntador].numMar); //Añadir número de marco
-                    this.marcos[this.paginasEnMp[this.apuntador].numMar] = true; //Liberar marco
-                    UIEjecucion.celdas[this.paginasEnMp[this.apuntador].numMar].setBackground(Color.green);
-                    Proceso p = this.getProceso(this.paginasEnMp[this.apuntador].idProceso);
-                    p.getETP(this.paginasEnMp[this.apuntador].numPag).setP(false); //Ya la página no se encuentra en mp
-                    p.getETP(this.paginasEnMp[this.apuntador].numPag).u = false; //Por ende u = false
-                    if(p.getPaginasEnMp() == 0){
+                    marco.add(PlanificadorMid.apuntador); //Añadir número del marco
+                    Proceso p = OS.getProceso(OS.marcos[PlanificadorMid.apuntador].getPagina().idProceso); //Proceso cuya página está en el marco
+                    UIEjecucion.celdas[PlanificadorMid.apuntador].setBackground(Color.green); //Visual
+                    p.getETP(OS.marcos[PlanificadorMid.apuntador].getPagina().numPag).setP(false); //Ya la página no se encuentra en mp
+                    p.setMarcoToETP(-1, OS.marcos[PlanificadorMid.apuntador].getPagina().numPag); //Desasignar marco
+                    OS.marcos[PlanificadorMid.apuntador].setLibre(true); //Liberar marco en mp
+                    /*if(p.getPaginasEnMp() == 0){
                         p.setEstado(false, true, false); //Estado suspendido
-                    }
-                    this.paginasEnMp[this.apuntador] = null;
+                    }*/
+                    OS.actualizarProceso(p);
+                    OS.marcos[PlanificadorMid.apuntador].setPagina(null);
                 }
             }
-            this.apuntador = (this.apuntador+1)%this.marcos.length;
+            PlanificadorMid.apuntador = (PlanificadorMid.apuntador+1)%OS.marcos.length;
         }
     }
     
-    public void leerPag(){
+        //Ingresar n páginas de un proceso
+    public static void escribirPag(){
         
     }
     
         //Hay marcos vacíos y cuantos son
-    private int getMarcosVacios(){
-        boolean vacio = true;
+    private static int getMarcosVacios(){
         int cantidad = 0;
-        for(int i=0;i<this.marcos.length;i++){
-            if(this.marcos[i] == true){
+        for(int i=0;i<OS.marcos.length;i++){
+            if(OS.marcos[i].getLibre() == true){
                 cantidad++;
             }
         }
@@ -181,51 +147,5 @@ public class PlanificadorMid {
             return cantidad; //Hay al menos un marco vacío
         }
     }
-    
-    public double getRestanteMs(){
-        double ocu = 0;
-        Object[] aux = this.procesos.toArray();
-        for(int i=0;i<aux.length;i++){
-            ocu = ocu + ((Proceso)aux[i]).getTam();
-        }
-        ocu = this.tamMS-ocu;
-        return ocu;
-    }
-    
-    public int getNumProcesos(){
-        return this.procesos.size();
-    }
-    
-    public Proceso getProceso(String i){
-        if(this.procesos.size()>0){
-            Object[] aux = this.procesos.toArray();
-            for(int j=0;j<aux.length;j++){
-                if(i.equals(((Proceso)aux[j]).getId())){
-                    return (Proceso)aux[j];
-                }
-            }
-        }
-        return null;
-    }
-    
-    public int getNumMarcos(){
-        return this.marcos.length;
-    }
-    
-    public double getTamMarco(){
-        return this.tamMarco;
-    }
-    
-    public double getTamMP(){
-        return this.tamMP;
-    }
-    
-    public double getTamMS(){
-        return this.tamMS;
-    }
-    
-    public boolean getMarcoValue(int i){
-        return this.marcos[i];
-    }
-    
+
 }
