325460ca2c8cf33366e8230975aa989b5b212f2a
SamuelVera <samuelvera1999@gmail.com>
2018-11-16 04:07:18 -0400

    Ajuste en los métodos de la ETP

--- src/logica/ETP.java
+++ src/logica/ETP.java
@@ -4,7 +4,7 @@ public class ETP { //Entrada de la tabla de página
     private boolean p; //Presente en memoria principal
     boolean u; //Referenciado recientemente
     int numMar; //Número de marco asociada
-    int numPag;
+    int numPag; //Número de pagina del proceso
     String idProceso;
     
     public ETP(int i, String id){
@@ -20,6 +20,14 @@ public class ETP { //Entrada de la tabla de página
     
     public boolean getP(){
         return this.p;
+    }
+    
+    public int getMarco(){
+        if(this.p){
+            return this.numMar;
+        }else{
+            return -1;
+        }
     }
     
 }
