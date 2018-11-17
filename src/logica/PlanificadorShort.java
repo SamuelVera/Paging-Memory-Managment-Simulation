d35a6817bf05b7143dac8553eb895d589c35e9e9
SamuelVera <samuelvera1999@gmail.com>
2018-11-17 02:44:59 -0400

    Hotfix del planificador a corto plazo

--- src/logica/PlanificadorShort.java
+++ src/logica/PlanificadorShort.java
@@ -1,44 +1,74 @@
 package logica;
 
 import java.util.LinkedList;
+import java.util.concurrent.Semaphore;
 
 public class PlanificadorShort{
 
     private static final LinkedList colaListos = new LinkedList(); //Lista de procesos listos
     protected static int state ;
+    private static Semaphore accessSema = new Semaphore(1);
     
-    protected static void aquireCPU() {
+    protected static void aquireCPUPrio() {
+        PlanificadorShort.acquirePrio();
+    }
+    
+    protected static void aquireCPU(){
         PlanificadorShort.acquire();
     }
 
-    protected static void releaseCPU() {
+    protected static void releaseCPU() throws InterruptedException {
         PlanificadorShort.release();
     }
-  
+    
     private static void acquire(){
         Object object = new Object();
 
         synchronized (object) {
             try {
+                PlanificadorShort.accessSema.acquire();
+                PlanificadorShort.state--;
+                if (PlanificadorShort.state < 0) {
+                    PlanificadorShort.colaListos.addLast(object);
+                    PlanificadorShort.accessSema.release();
+                    object.wait();                
+                }else{
+                    PlanificadorShort.accessSema.release();
+                }
+            } catch (InterruptedException ie) {
+            }
+        }
+    }
+  
+    private static void acquirePrio(){
+        Object object = new Object();
+        synchronized (object) {
+            try {
+                PlanificadorShort.accessSema.acquire();
                 PlanificadorShort.state--;
                 if (PlanificadorShort.state < 0) {
                     PlanificadorShort.colaListos.addFirst(object);
+                    PlanificadorShort.accessSema.release();
                     object.wait();                
+                }else{
+                    PlanificadorShort.accessSema.release();
                 }
             } catch (InterruptedException ie) {
             }
         }
     }
     
-    private static void release() {
+    private static void release() throws InterruptedException {
+        PlanificadorShort.accessSema.acquire();
         PlanificadorShort.state++;
         if(PlanificadorShort.state <= 0){
             Object object = PlanificadorShort.colaListos.removeFirst();
+            PlanificadorShort.accessSema.release();
             synchronized (object) {
                 object.notify();
             }
+        }else{
+            PlanificadorShort.accessSema.release();
         }
-        
     }
-    
 }
